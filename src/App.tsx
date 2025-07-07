77848338594bbe13221d8e6f8f30ac1a734e9082
nima moradi <mnima8100@gmail.com>
2025-07-07 19:03:09 +0330

    Refactor App component to implement dark mode theme and organize layout with Navbar, Sidebar, Feed, Rightbar, and Add components

--- src/App.tsx
+++ src/App.tsx
@@ -2,18 +2,35 @@ import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-import { ThemeProvider } from '@mui/material';
-import { theme } from './theme';
+import Sidebar from "./components/Sidebar";
+import Feed from "./components/Feed";
+import Rightbar from "./components/Rightbar";
+import { Box, createTheme, Stack, ThemeProvider } from "@mui/material";
+import Navbar from "./components/Navbar";
+import Add from "./components/Add";
+import { useState } from "react";
 
 function App() {
+  const [mode, setMode] = useState<"light" | "dark">("light");
 
+  const darkTheme = createTheme({
+    palette: {
+      mode: mode,
+    },
+  });
   return (
-    <>
-     <ThemeProvider theme={theme}>
-    
-     </ThemeProvider>
-    </>
-  )
+    <ThemeProvider theme={darkTheme}>
+      <Box bgcolor={"background.default"} color={"text.primary"}>
+        <Navbar />
+        <Stack direction="row" spacing={2} justifyContent="space-between">
+        <Sidebar setMode={setMode} mode={mode}/>
+          <Feed />
+          <Rightbar />
+        </Stack>
+        <Add />
+      </Box>
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
